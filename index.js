30b43202464101e44a2fcd46f8c73ce9dee695d4
pirofestionalDev <r4roggit1@gmail.com>
2024-10-15 15:07:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let user = window.alert("Having a good day?");
+
+document.getElementById("mySubmit").onclick = function(){
+    user = document.getElementById("myIp").value;
+    // user = document.getElementById("mySubmit").onclick = function(){
+    //     console.log("");
+    // }
+    document.getElementById("myH1").textContent = `Welcome ${user},`
+}
+//Noob i am.
+// const pi=22/7;
+// let raduis;
+// let circumference;
+// let res;
+
+// document.getElementById(`calc`).onclick=function(){
+//     raduis=document.getElementById(`Ip`).ariaValueMax;
+// }
+// raduis=Number(raduis);
+
+// circumference=2*pi*raduis;
+// console.log(circumference);
+
+const pi = 22/7;
+let radius;
+let circumference;
+
+document.getElementById('calc').onclick = function() {
+    radius = document.getElementById('Ip').value;
+    radius = Number(radius);
+
+    if (!isNaN(radius) && radius > 0) {
+        circumference = 2 * pi * radius;
+        document.getElementById('Op').value = `Answer: ${circumference.toFixed(3)}`;
+    } else {
+        document.getElementById('Op').value = 'Invalid input. Please enter a valid radius.';
+    }
+};
